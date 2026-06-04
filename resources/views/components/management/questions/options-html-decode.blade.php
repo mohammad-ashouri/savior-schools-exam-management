017f039bc1d0eb9f0da46c68b623293b183a8a2c
afra <m.ashouri.wdev@gmail.com>
2026-06-04 11:23:29 +0330

    Change option numbers to ABCD in questions management

--- resources/views/components/management/questions/options-html-decode.blade.php
+++ resources/views/components/management/questions/options-html-decode.blade.php
@@ -1,7 +1,11 @@
 <div style="display: flex; flex-wrap: wrap; gap: 20px; align-items: center;">
+    @php
+        $letters = ['A', 'B', 'C', 'D'];
+        $index = 0;
+    @endphp
     @foreach($options as $option)
         <div style="display: flex; align-items: center; gap: 5px;">
-            <span>{{ count($options)>1 ? "$loop->iteration- " : '' }}</span>
+            <span>{{ count($options)>1 ? $letters[$index++]."- " : '' }}</span>
             <span>{!! strip_tags($option, '<sup><sub><img>') !!}</span>
         </div>
     @endforeach
